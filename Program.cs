25340d6d084ecc04ebab62fa6f1955f83ffcd9f9
Vadim <01prosto01@mail.ru>
2023-05-21 13:55:49 +1000

    1st task done

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿Console.WriteLine("Type first integer number");
+﻿// 1st TASK ______________________________________________________
+Console.WriteLine("Type first integer number");
 int number01 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(number01);
+
+Console.WriteLine("Type second integer number");
+int number02 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("You typed the following numbers: " + number01 + " " + number02);
+Console.WriteLine("Let's find the biggest");
+
+
+if (number01 > number02)
+{
+    Console.WriteLine("The number: " + number01 + " -is the biggest number!");
+}
+else 
+{
+    Console.WriteLine("The number: " + number02 + " -is the biggest number!");
+}
+//________________________________________________________________
+
+// 2nd TASK ______________________________________________________
