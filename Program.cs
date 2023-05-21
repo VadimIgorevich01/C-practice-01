28c8d7a17f41ef68e1feb2c4caf2eb61aa3a3015
Vadim <01prosto01@mail.ru>
2023-05-21 13:33:40 +1000

    1 task in progress2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Type first integer number");
+int number01 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number01);
